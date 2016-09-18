a3efd77f02518078d6592d9bcb5c7baaec69b398
Junerver <junerver@gmail.com>
2016-09-18 16:50:55 +0800

    init

---
+++ app/src/main/java/com/xxzx/wifisocketdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.xxzx.wifisocketdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.etContent)
+    EditText mEtContent;
+    @BindView(R.id.btnSend)
+    Button mBtnSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnSend)
+    public void onClick() {
+    }
+}
